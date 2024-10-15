02c52b29ed07431199aacaea2b842242c8aa95b6
Rafael Tomelin <rafael.tomelin@gmail.com>
2024-10-15 08:56:41 -0300

    fix: :bug: update the branch in cloud modules

--- infra/aws/module.tf
+++ infra/aws/module.tf
@@ -1,7 +1,7 @@
 
 
 module "ascode" {
-  source = "git::https://github.com/kubediscovery/platform-scaffolder.git//terraform/modules/platform/labs/aws?ref=aws"
+  source = "git::https://github.com/kubediscovery/platform-scaffolder.git//terraform/modules/platform/labs/aws?ref=develop"
 
   project_name = local.yaml_configs.name
   cidr_block   = local.yaml_configs.cidr
