7b7cc266fd42dc33f793e00c74ab367a89050f1e
Tomelin <rafael.tomelin@gmail.com>
2024-10-15 19:07:41 -0300

    Update module.tf

--- terraform/modules/platform/labs/aws/module.tf
+++ terraform/modules/platform/labs/aws/module.tf
@@ -1,11 +1,7 @@
 
 
 module "platform" {
-<<<<<<< HEAD
-  source = "git::https://github.com/kubediscovery/platform-infrastructure.git//terraform/modules/aws/eks_cluster?ref=aws"
-=======
   source = "git::https://github.com/kubediscovery/platform-infrastructure.git//modules/aws/eks_cluster?ref=develop"
->>>>>>> b1a9b92 (fix: :bug: update branch at modules)
 
   project_name = var.project_name
   cidr_block   = var.cidr_block
@@ -14,11 +10,7 @@ module "platform" {
 }
 
 # module "addons" {
-<<<<<<< HEAD
-#   source = "git::https://github.com/kubediscovery/platform-infrastructure.git//terraform/modules/addons/init/?ref=aws"
-=======
 #   source = "git::https://github.com/kubediscovery/platform-infrastructure.git//modules/addons/init/?ref=develop"
->>>>>>> b1a9b92 (fix: :bug: update branch at modules)
 
 #   project_name           = var.project_name
 #   cluster_endpoint       = module.platform.cluster.endpoint
