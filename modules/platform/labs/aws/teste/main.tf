187c992e4f03962dcdd1c9525f38e4f77d0d2582
Rafael Tomelin <rafael.tomelin@gmail.com>
2024-10-15 20:24:10 -0300

    update

--- modules/platform/labs/aws/teste/main.tf
+++
@@ -1,4 +0,0 @@
-variable "teste" {
-  type = string
-  default = "teste"
-}
---
+++ terraform/modules/platform/labs/aws/module.tf
@@ -0,0 +1,9 @@
+
+module "k8s_cluster" {
+  source = "git::https://github.com/kubediscovery/platform-scaffolder.git//modules/aws/eks_cluster?ref=develop"
+
+  project_name = var.project_name
+  cidr_block   = var.cidr_block
+  tags         = var.tags
+  region       = var.region
+}
