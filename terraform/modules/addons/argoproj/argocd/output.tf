91cc93ee91d170f88ff6d3f19c5c17234233070b
Rafael Tomelin <rafael.tomelin@gmail.com>
2024-10-16 13:36:48 -0300

    update terraform files

--- terraform/modules/addons/argoproj/argocd/output.tf
+++ terraform/modules/addons/argoproj/argocd/output.tf
@@ -4,7 +4,7 @@ output "initial_secret" {
 
 output "external_address" {
   value = {
-    "ingress" = data.kubernetes_ingress_v1.argocd_server.spec.0.rule
+    "ingress" = data.kubernetes_ingress_v1.argocd_server.spec.0.rule.0.host
     "hostname" =  data.kubernetes_ingress_v1.argocd_server.status.0.load_balancer.0.ingress.0.hostname
     "ip" = data.kubernetes_ingress_v1.argocd_server.status.0.load_balancer.0.ingress.0.ip
   }
