469166656de4d860769df2f4a5b055609babb40e
Rafael Tomelin <rafael.tomelin@gmail.com>
2024-10-15 22:21:30 -0300

    update terraform files

--- terraform/modules/addons/init/module.tf
+++ terraform/modules/addons/init/module.tf
@@ -13,8 +13,10 @@
 # }
 
 module "argocd" {
-  source       = "git::https://github.com/kubediscovery/platform-scaffolder.git//terraform/modules/addons/argoproj/argocd/?ref=develop"
-  count        = var.enabled_addons.argoproj-argocd ? 1 : 0
+  source = "git::https://github.com/kubediscovery/platform-scaffolder.git//terraform/modules/addons/argoproj/argocd/?ref=develop"
+  count  = (!isempty(var.enabled_addons) && var.enabled_addons["argoproj-argocd"]) ? 1 : 0
+
+
   labels       = var.tags
   project_name = var.project_name
 }
