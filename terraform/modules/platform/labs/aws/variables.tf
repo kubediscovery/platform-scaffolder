c519c4c6310e604124ac174ac4d8241701fef612
Rafael Tomelin <rafael.tomelin@gmail.com>
2024-10-16 07:42:02 -0300

    update terraform files

--- terraform/modules/platform/labs/aws/variables.tf
+++ terraform/modules/platform/labs/aws/variables.tf
@@ -92,7 +92,7 @@ variable "cluster_group_version" {
 }
 
 variable "enabled_addons" {
-  type        = map(bool)
-  description = "Enable addons for the EKS cluster"
+  type        = map(map(any))
+  description = "Enabled addons for the cluster"
   default     = {}
 }
