f1fe589910a438996672aa36a11173ce11445021
Rafael Tomelin <rafael.tomelin@gmail.com>
2024-10-16 18:18:55 -0300

    feat: enabled csi driver

--- terraform/modules/aws/eks_cluster/clusters/iam.tf
+++ terraform/modules/aws/eks_cluster/clusters/iam.tf
@@ -38,7 +38,7 @@ resource "aws_iam_role_policy_attachment" "eks_service_policy_attachment" {
 
 resource "aws_iam_role_policy_attachment" "eks_csi_policy_attachment" {
   role       = aws_iam_role.eks_cluster_role.name
-  policy_arn = "arn:aws:iam::931627500555:policy/AmazonEKS_EFS_CSI_Driver_Policy"
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_EFS_CSI_Driver_Policy"
 }
 
 resource "aws_iam_policy" "eks_cluster_ebs_policy" {
