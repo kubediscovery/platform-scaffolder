2e49db2339e8890e38cfa93172fb46dc5ac7a785
Rafael Tomelin <rafael.tomelin@gmail.com>
2024-10-16 07:37:08 -0300

    update terraform files

--- terraform/modules/addons/init/module.tf
+++ terraform/modules/addons/init/module.tf
@@ -13,20 +13,42 @@
 # }
 
 locals {
+  empty_addons = {
+    "metrics-server" = {
+      enabled = false
+    }
+    "cluster-autoscaler" = {
+      enabled = false
+    }
+    "aws-node-termination-handler" = {
+      enabled = false
+    }
+    "argoproj-argocd" = {
+      enabled = false
+    }
+    "argoproj-rollout" = {
+      enabled = false
+    }
+    "kong-ingress-controller" = {
+      enabled = false
+    }
+  }
+
   enabled_addons = length(var.enabled_addons) > 0 ? var.enabled_addons : {
-    "metrics-server"               = contains(keys(var.enabled_addons), "metrics-server") ? var.enabled_addons.metrics-server : false
-    "cluster-autoscaler"           = contains(keys(var.enabled_addons), "cluster-autoscaler") ? var.enabled_addons.cluster-autoscaler : false
-    "aws-node-termination-handler" = contains(keys(var.enabled_addons), "aws-node-termination-handler") ? var.enabled_addons.aws-node-termination-handler : false
-    "argoproj-argocd"              = contains(keys(var.enabled_addons), "argoproj-argocd") ? var.enabled_addons.argoproj-argocd : false
-    "argoproj-rollout"             = contains(keys(var.enabled_addons), "argoproj-rollout") ? var.enabled_addons.argoproj-rollout : false
-    "kong-ingress-controller"      = contains(keys(var.enabled_addons), "kong-ingress-controller") ? var.enabled_addons.metkong-ingress-controller : false
+    "metrics-server"               = contains(keys(var.enabled_addons), "metrics-server") ? var.enabled_addons.metrics-server : local.empty_addons.metrics-server
+    "cluster-autoscaler"           = contains(keys(var.enabled_addons), "cluster-autoscaler") ? var.enabled_addons.cluster-autoscaler : local.empty_addons.cluster-autoscaler
+    "aws-node-termination-handler" = contains(keys(var.enabled_addons), "aws-node-termination-handler") ? var.enabled_addons.aws-node-termination-handler : local.empty_addons.aws-node-termination-handler
+    "argoproj-argocd"              = contains(keys(var.enabled_addons), "argoproj-argocd") ? var.enabled_addons.argoproj-argocd : local.empty_addons.argoproj-argocd
+    "argoproj-rollout"             = contains(keys(var.enabled_addons), "argoproj-rollout") ? var.enabled_addons.argoproj-rollout : local.empty_addons.argoproj-rollout
+    "kong-ingress-controller"      = contains(keys(var.enabled_addons), "kong-ingress-controller") ? var.enabled_addons.metkong-ingress-controller : local.empty_addons.kong-ingress-controller
   }
 }
 
 module "argocd" {
   source = "git::https://github.com/kubediscovery/platform-scaffolder.git//terraform/modules/addons/argoproj/argocd/?ref=develop"
-  count  = local.enabled_addons.argoproj-argocd ? 1 : 0
+  count  = local.enabled_addons.argoproj-argocd.enabled ? 1 : 0
 
-  labels       = var.tags
-  project_name = var.project_name
+  labels        = var.tags
+  chart_version = lookup(local.enabled_addons["argoproj-argocd"], "version", "7.5.2")
+  project_name  = var.project_name
 }
