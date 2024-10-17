f904658142c5f97e3ad804319f091f967515f3d5
Rafael Tomelin <rafael.tomelin@gmail.com>
2024-10-16 22:44:37 -0300

    feat: enabled csi driver

--- terraform/modules/addons/kong/locals.tf
+++ terraform/modules/addons/kong/locals.tf
@@ -10,7 +10,7 @@ locals {
       "type"    = try(data.kubernetes_ingress_v1.kong_portal.status.0.load_balancer.0.ingress.0.ip != "" ? "address" : "hostname", "")
     },
     {
-      "name"    = data.kubernetes_ingress_v1.kong_portalapi.spec == "" ? "" : data.kubernetes_ingress_v1.kong_portalapi.spec.0.rule.0.host
+      "name"    = data.kubernetes_ingress_v1.kong_portalapi.spec == null ? "" : data.kubernetes_ingress_v1.kong_portalapi.spec.0.rule.0.host
       "address" = data.kubernetes_ingress_v1.kong_portalapi.status == null ? "" : try(data.kubernetes_ingress_v1.kong_portalapi.status.0.load_balancer.0.ingress.0.hostname, data.kubernetes_ingress_v1.kong_portalapi.status.0.load_balancer.0.ingress.0.ip)
       "type"    = data.kubernetes_ingress_v1.kong_portalapi.status == null ? "" : data.kubernetes_ingress_v1.kong_portalapi.status.0.load_balancer.0.ingress.0.ip != "" ? "address" : "hostname"
     },
