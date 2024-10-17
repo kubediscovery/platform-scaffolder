a04163bd88a0afb714bc6a00876089a7f018ae53
Rafael Tomelin <rafael.tomelin@gmail.com>
2024-10-16 22:43:31 -0300

    feat: enabled csi driver

--- terraform/modules/addons/init/module.tf
+++ terraform/modules/addons/init/module.tf
@@ -71,13 +71,13 @@ module "kong" {
 }
 
 module "kong_publish" {
-  source   = "git::https://github.com/kubediscovery/platform-scaffolder.git//terraform/modules/addons/cloudflare/?ref=develop"
+  source = "git::https://github.com/kubediscovery/platform-scaffolder.git//terraform/modules/addons/cloudflare/?ref=develop"
 
   api_token      = var.cloudflare_api_token
   zone_id        = var.cloudflare_zone_id
   record_type    = module.kong[0].publish[0].type == "address" ? "A" : "CNAME"
-  record_name    =  module.kong[0].publish[0].name
-  record_address =  module.kong[0].publish[0].address
+  record_name    = module.kong[0].publish[0].name
+  record_address = module.kong[0].publish[0].address
 }
 
 module "atlantis" {
