84d81d7459fe05ccacce72d9f7f9bff78a979886
Rafael Tomelin <rafael.tomelin@gmail.com>
2024-10-17 18:43:56 -0300

    update

--- terraform/modules/aws/eks_cluster/volume/storage.tf
+++ terraform/modules/aws/eks_cluster/volume/storage.tf
@@ -5,7 +5,9 @@ resource "kubernetes_storage_class_v1" "efs_sc" {
     labels = var.tags
   }
   storage_provisioner = var.storage_provisioner
-  reclaim_policy      = "Retain"
+  reclaim_policy      = "Delete"
+  volume_binding_mode = "Immediate"
+  allow_volume_expansion = false  
 }
 
 resource "kubernetes_persistent_volume_v1" "efs_pv" {
@@ -22,7 +24,7 @@ resource "kubernetes_persistent_volume_v1" "efs_pv" {
       "ReadWriteOnce"
     ]
     volume_mode                      = "Filesystem"
-    persistent_volume_reclaim_policy = "Retain"
+    persistent_volume_reclaim_policy = "Delete"
     storage_class_name               = kubernetes_storage_class_v1.efs_sc.metadata.0.name
     persistent_volume_source {
 
@@ -42,7 +44,6 @@ resource "kubernetes_persistent_volume_claim_v1" "this" {
   }
   spec {
     access_modes = [
-      "ReadWriteMany",
       "ReadWriteOnce"
     ]
     resources {
@@ -50,6 +51,8 @@ resource "kubernetes_persistent_volume_claim_v1" "this" {
         storage = "1Gi"
       }
     }
+    volume_mode = "Filesystem"
+    volume_name = kubernetes_persistent_volume_v1.efs_pv.metadata.0.name
     storage_class_name = kubernetes_storage_class_v1.efs_sc.metadata.0.name
   }
 }
