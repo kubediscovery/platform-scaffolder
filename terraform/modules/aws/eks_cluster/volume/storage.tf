8152631b6e6c16182c4701760ffd54cfd0de771d
Rafael Tomelin <rafael.tomelin@gmail.com>
2024-10-17 17:41:12 -0300

    update

--- terraform/modules/aws/eks_cluster/volume/storage.tf
+++ terraform/modules/aws/eks_cluster/volume/storage.tf
@@ -1,31 +1,31 @@
 
 resource "kubernetes_storage_class_v1" "efs_sc" {
   metadata {
-    name = "efs-sc"
+    name = var.storage_class_name
   }
-  storage_provisioner = "efs.csi.aws.com"
+  storage_provisioner = var.storage_provisioner
   reclaim_policy      = "Retain"
 }
 
 resource "kubernetes_persistent_volume_v1" "efs_pv" {
   metadata {
-    name = "efs-pv"
+    name = var.persistent_volume_name
   }
   spec {
     capacity = {
-      storage = "2Gi"
+      storage = var.storage_capacity_size
     }
     access_modes = [
       "ReadWriteMany",
       "ReadWriteOnce"
-      ]
-      volume_mode = "Filesystem"
-      persistent_volume_reclaim_policy = "Retain"
-      storage_class_name = kubernetes_storage_class_v1.efs_sc.metadata.0.name
+    ]
+    volume_mode                      = "Filesystem"
+    persistent_volume_reclaim_policy = "Retain"
+    storage_class_name               = kubernetes_storage_class_v1.efs_sc.metadata.0.name
     persistent_volume_source {
-      
+
       csi {
-        driver = "efs.csi.aws.com"
+        driver        = kubernetes_storage_class_v1.efs_sc.storage_provisioner
         volume_handle = "EFS_VOLUME_ID"
       }
     }
---
+++ terraform/modules/aws/eks_cluster/volume/variables.tf
@@ -0,0 +1,23 @@
+variable "storage_class_name" {
+  type        = string
+  default     = "efs-sc"
+  description = "Storage class name for the volume to be created"
+}
+
+variable "persistent_volume_name" {
+  type        = string
+  default     = "efs-pv"
+  description = "Persistent volume name for the volume to be created in the cluster"
+}
+
+variable "storage_capacity_size" {
+  type        = string
+  default     = "2Gi"
+  description = "Storage capacity size for the volume to be created"
+}
+
+variable "storage_provisioner" {
+  type        = string
+  default     = "efs.csi.aws.com"
+  description = "Storage provisioner for the volume to be created"
+}
