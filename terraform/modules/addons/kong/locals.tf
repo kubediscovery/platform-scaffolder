07fe03f37047947f433f32702cfb47a4d0123fcd
Rafael Tomelin <rafael.tomelin@gmail.com>
2024-10-16 22:48:44 -0300

    feat: enabled csi driver

--- terraform/modules/addons/kong/locals.tf
+++ terraform/modules/addons/kong/locals.tf
@@ -1,13 +1,13 @@
 locals {
   publish = [{
-    "name"    = try(data.kubernetes_ingress_v1.kong_manager.spec.0.rule.0.host, "")
-    "address" = try(data.kubernetes_ingress_v1.kong_manager.status.0.load_balancer.0.ingress.0.hostname, data.kubernetes_ingress_v1.kong_manager.status.0.load_balancer.0.ingress.0.ip)
-    "type"    = try(data.kubernetes_ingress_v1.kong_manager.status.0.load_balancer.0.ingress.0.ip != "" ? "address" : "hostname", "")
+    "name"    = data.kubernetes_ingress_v1.kong_manager.spec == null ? "" : data.kubernetes_ingress_v1.kong_manager.spec.0.rule.0.host
+    "address" = data.kubernetes_ingress_v1.kong_manager.status == null ? "" : try(data.kubernetes_ingress_v1.kong_manager.status.0.load_balancer.0.ingress.0.hostname, data.kubernetes_ingress_v1.kong_manager.status.0.load_balancer.0.ingress.0.ip)
+    "type"    = data.kubernetes_ingress_v1.kong_manager.status == null ? "" : data.kubernetes_ingress_v1.kong_manager.status.0.load_balancer.0.ingress.0.ip != "" ? "address" : "hostname"
     },
     {
-      "name"    = try(data.kubernetes_ingress_v1.kong_portal.spec.0.rule.0.host, "")
-      "address" = try(data.kubernetes_ingress_v1.kong_portal.status.0.load_balancer.0.ingress.0.hostname, data.kubernetes_ingress_v1.kong_portal.status.0.load_balancer.0.ingress.0.ip)
-      "type"    = try(data.kubernetes_ingress_v1.kong_portal.status.0.load_balancer.0.ingress.0.ip != "" ? "address" : "hostname", "")
+      "name"    = data.kubernetes_ingress_v1.kong_portal.spec == null ? "" : data.kubernetes_ingress_v1.kong_portal.spec.0.rule.0.host
+      "address" = data.kubernetes_ingress_v1.kong_portal.status == null ? "" : try(data.kubernetes_ingress_v1.kong_portal.status.0.load_balancer.0.ingress.0.hostname, data.kubernetes_ingress_v1.kong_portal.status.0.load_balancer.0.ingress.0.ip)
+      "type"    = data.kubernetes_ingress_v1.kong_portal.status == null ? "" : data.kubernetes_ingress_v1.kong_portal.status.0.load_balancer.0.ingress.0.ip != "" ? "address" : "hostname"
     },
     {
       "name"    = data.kubernetes_ingress_v1.kong_portalapi.spec == null ? "" : data.kubernetes_ingress_v1.kong_portalapi.spec.0.rule.0.host
@@ -15,8 +15,8 @@ locals {
       "type"    = data.kubernetes_ingress_v1.kong_portalapi.status == null ? "" : data.kubernetes_ingress_v1.kong_portalapi.status.0.load_balancer.0.ingress.0.ip != "" ? "address" : "hostname"
     },
     {
-      "name"    = try(data.kubernetes_ingress_v1.kong_admin.spec.0.rule.0.host, "")
-      "address" = try(data.kubernetes_ingress_v1.kong_admin.status.0.load_balancer.0.ingress.0.hostname, data.kubernetes_ingress_v1.kong_admin.status.0.load_balancer.0.ingress.0.ip)
-      "type"    = try(data.kubernetes_ingress_v1.kong_admin.status.0.load_balancer.0.ingress.0.ip != "" ? "address" : "hostname", "")
+      "name"    = data.kubernetes_ingress_v1.kong_admin.spec == null ? "" : data.kubernetes_ingress_v1.kong_admin.spec.0.rule.0.host
+      "address" = data.kubernetes_ingress_v1.kong_admin.status == null ? "" : try(data.kubernetes_ingress_v1.kong_admin.status.0.load_balancer.0.ingress.0.hostname, data.kubernetes_ingress_v1.kong_admin.status.0.load_balancer.0.ingress.0.ip)
+      "type"    = data.kubernetes_ingress_v1.kong_admin.status == null ? "" : data.kubernetes_ingress_v1.kong_admin.status.0.load_balancer.0.ingress.0.ip != "" ? "address" : "hostname"
   }]
 }
