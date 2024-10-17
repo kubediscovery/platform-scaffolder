8aacb489a62f5f55f23df660e0877aa84d539f18
Rafael Tomelin <rafael.tomelin@gmail.com>
2024-10-17 10:28:46 -0300

    update

--- terraform/modules/addons/argoproj/argocd/output.tf
+++ terraform/modules/addons/argoproj/argocd/output.tf
@@ -8,7 +8,7 @@ output "publish" {
   value = {
     "name"    = data.kubernetes_ingress_v1.argocd_server.spec == null ? "" : data.kubernetes_ingress_v1.argocd_server.spec.0.rule.0.host
     "address" = data.kubernetes_ingress_v1.argocd_server.status == null ? "" : try(data.kubernetes_ingress_v1.argocd_server.status.0.load_balancer.0.ingress.0.hostname, data.kubernetes_ingress_v1.argocd_server.status.0.load_balancer.0.ingress.0.ip)
-    "type"    = data.kubernetes_ingress_v1.argocd_server.status == null ? "" : data.kubernetes_ingress_v1.argocd_server.status.0.load_balancer.0.ingress.0.ip != "" ? "address" : "hostname"
+    "type"    = data.kubernetes_ingress_v1.argocd_server.status == null ? "" : (data.kubernetes_ingress_v1.argocd_server.status.0.load_balancer.0.ingress.0.ip != "" ? "address" : "hostname")
   }
   description = "External address to access the ArgoCD server"
   sensitive   = false
