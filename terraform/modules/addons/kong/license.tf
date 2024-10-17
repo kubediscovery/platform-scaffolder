6590a8f6efff558d00380537d2337a10bcb0cb92
Rafael Tomelin <rafael.tomelin@gmail.com>
2024-10-17 17:17:24 -0300

    update

---
+++ terraform/modules/addons/kong/license.tf
@@ -0,0 +1,17 @@
+resource "kubernetes_namespace_v1" "kong" {
+  metadata {
+    name   = var.namespace
+    labels = var.labels
+  }
+}
+
+resource "kubernetes_secret_v1" "license" {
+  metadata {
+    name      = var.name
+    namespace = kube_namespace_v1.kong.metadata.0.name
+    labels    = var.labels
+  }
+  data = {
+    license = "'{}'"
+  }
+}
