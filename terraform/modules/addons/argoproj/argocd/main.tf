06b97908b29147c5b74f60449d0054f593de8714
Rafael Tomelin <rafael.tomelin@gmail.com>
2024-10-18 08:06:27 -0300

    update

--- terraform/modules/addons/argoproj/argocd/main.tf
+++ terraform/modules/addons/argoproj/argocd/main.tf
@@ -7,9 +7,9 @@
  */
 locals {
    publish = {
-    "name"    = data.kubernetes_ingress_v1.argocd_server.spec == null ? "ok" : ""
-    "address" = data.kubernetes_ingress_v1.argocd_server.status == null ? "ok" : ""
-    "type"    = data.kubernetes_ingress_v1.argocd_server.status == null ? "ok" : ""
+    "name"    = data.kubernetes_ingress_v1.argocd_server.spec == null ? "" : data.kubernetes_ingress_v1.argocd_server.spec.0.rules.0.host
+    "address" = data.kubernetes_ingress_v1.argocd_server.status == null ? "" : try(data.kubernetes_ingress_v1.argocd_server.status.0.load_balancer.0.ingress.0.hostname, data.kubernetes_ingress_v1.argocd_server.status.0.load_balancer.0.ingress.0.ip)
+    "type"    = data.kubernetes_ingress_v1.argocd_server.status == null ? "" : data.kubernetes_ingress_v1.argocd_server.status.0.load_balancer.0.ingress.0.ip != "" ? "address" : "hostname"
   }
 }
 
@@ -29,13 +29,6 @@ resource "helm_release" "argocd" {
     project_name  = var.project_name
     ingress_class = var.ingress_class
   })]
-}
-
-data "kubernetes_service" "argocd_server" {
-  metadata {
-    name      = "argocd-server"
-    namespace = helm_release.argocd.namespace
-  }
 }
 
 data "kubernetes_secret" "argocd_server" {
