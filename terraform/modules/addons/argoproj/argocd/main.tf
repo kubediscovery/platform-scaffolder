b3ba12d27660d6163cc378807997c182b09e86e6
Rafael Tomelin <rafael.tomelin@gmail.com>
2024-10-18 08:07:10 -0300

    update

--- terraform/modules/addons/argoproj/argocd/main.tf
+++ terraform/modules/addons/argoproj/argocd/main.tf
@@ -7,7 +7,7 @@
  */
 locals {
    publish = {
-    "name"    = data.kubernetes_ingress_v1.argocd_server.spec == null ? "" : data.kubernetes_ingress_v1.argocd_server.spec.0.rules.0.host
+    "name"    = data.kubernetes_ingress_v1.argocd_server.spec == null ? "" : data.kubernetes_ingress_v1.argocd_server.spec.0.rule.0.host
     "address" = data.kubernetes_ingress_v1.argocd_server.status == null ? "" : try(data.kubernetes_ingress_v1.argocd_server.status.0.load_balancer.0.ingress.0.hostname, data.kubernetes_ingress_v1.argocd_server.status.0.load_balancer.0.ingress.0.ip)
     "type"    = data.kubernetes_ingress_v1.argocd_server.status == null ? "" : data.kubernetes_ingress_v1.argocd_server.status.0.load_balancer.0.ingress.0.ip != "" ? "address" : "hostname"
   }
