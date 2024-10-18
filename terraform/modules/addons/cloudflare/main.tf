ff6c44c9b4b22d399443fd9f43bab310668ab56b
Rafael Tomelin <rafael.tomelin@gmail.com>
2024-10-18 08:25:02 -0300

    update

--- terraform/modules/addons/cloudflare/main.tf
+++ terraform/modules/addons/cloudflare/main.tf
@@ -1,20 +1,12 @@
 
-resource "local_file" "name" {
-  for_each = { for idx, record in var.dns_records : idx => record }
-  content = "\n ${each.value.record_name}-${each.value.record_address}-${each.value.record_type}"
-  filename = "${path.root}/name.txt"
-}
-
 resource "cloudflare_dns_record" "record" {  
 
-  for_each = { for idx, record in var.dns_records : idx => record if record.record_name != "" }
+  for_each = { for idx, record in var.dns_records : idx => record if record.record_name != "" && record.record_address != "" }
   
   zone_id = var.zone_id
   name    = each.value.record_name
   content = each.value.record_address
-  type    = each.value.record_type
+  type    = each.value.record_type == "address" ? "A" : "CNAME"
   proxied = var.record_proxy
   ttl     = var.record_ttl
-
-  depends_on = [ local_file.name ]
 }
