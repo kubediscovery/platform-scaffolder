cede69512fa3046b21b656e6c2781dcd5f2d214d
Rafael Tomelin <rafael.tomelin@gmail.com>
2024-10-18 10:16:26 -0300

    update

--- terraform/modules/switch/cloudflare/main.tf
+++ terraform/modules/switch/cloudflare/main.tf
@@ -1,23 +1,12 @@
-locals {
-  dns_records_map = {
-    for idx, record in var.dns_records : idx => {
-      name    = record.name
-      address = record.address
-      type    = record.type  == "address" ? "A" : "CNAME"
-    }
-    if record.name != "" && record.address != "" 
-  }
-}
 
+resource "cloudflare_dns_record" "record" {  
 
-# resource "cloudflare_dns_record" "record" {  
-
-#   for_each = local.dns_records_map
+  for_each = { for idx, record in var.dns_records : idx => record if record.name != "" && record.address != "" }
   
-#   zone_id = var.zone_id
-#   name    = each.value.name
-#   content = each.value.address
-#   type    = each.value.type == "address" ? "A" : "CNAME"
-#   proxied = var.record_proxy
-#   ttl     = var.record_ttl
-# }
+  zone_id = var.zone_id
+  name    = each.value.name
+  content = each.value.address
+  type    = each.value.type == "address" ? "A" : "CNAME"
+  proxied = var.record_proxy
+  ttl     = var.record_ttl
+}
