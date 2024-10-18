950226befa768488a61ca36eca7a8f41a06fdb1f
Rafael Tomelin <rafael.tomelin@gmail.com>
2024-10-17 23:41:31 -0300

    update

--- terraform/modules/addons/cloudflare/main.tf
+++ terraform/modules/addons/cloudflare/main.tf
@@ -11,7 +11,7 @@ resource "cloudflare_dns_record" "record" {
 
   zone_id = var.zone_id
   name    = var.record_name
-  content = "${create_record}-${var.record_address}"
+  content = "${local.create_record}-${var.record_address}"
   type    = var.record_type
   proxied = var.record_proxy
   ttl     = var.record_ttl
