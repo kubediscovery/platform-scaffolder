b43a51f2be49ef9dd1a646797350a56607dd35b0
Rafael Tomelin <rafael.tomelin@gmail.com>
2024-10-17 23:37:27 -0300

    update

--- terraform/modules/addons/cloudflare/main.tf
+++ terraform/modules/addons/cloudflare/main.tf
@@ -1,6 +1,8 @@
 
 resource "cloudflare_dns_record" "record" {
-  count = var.record_name != "" ? 1 : 0 && var.record_address != "" ? 1 : 0
+  # count = var.record_name != "" ? 1 : 0 && var.record_address != "" ? 1 : 0
+    count = length([for v in [var.record_name, var.record_address] : v if v != ""])
+
 
   zone_id = var.zone_id
   name    = var.record_name
