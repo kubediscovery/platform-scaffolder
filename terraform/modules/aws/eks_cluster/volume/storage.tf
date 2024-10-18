923200ad8d44cc124641a19720cb8cf9cef567c0
Rafael Tomelin <rafael.tomelin@gmail.com>
2024-10-17 21:43:30 -0300

    update

--- terraform/modules/aws/eks_cluster/volume/storage.tf
+++ terraform/modules/aws/eks_cluster/volume/storage.tf
@@ -34,7 +34,8 @@ resource "kubernetes_persistent_volume_v1" "efs_pv" {
     }
 
     access_modes = [
-      "ReadWriteOnce"
+      "ReadWriteOnce",
+      "ReadWriteMany"
     ]
     volume_mode                      = "Filesystem"
     persistent_volume_reclaim_policy = "Delete"
@@ -47,24 +48,3 @@ resource "kubernetes_persistent_volume_v1" "efs_pv" {
     }
   }
 }
-
-# resource "kubernetes_persistent_volume_claim_v1" "this" {
-#   metadata {
-#     name   = "efs-pvc"
-#     namespace = "storage"
-#     labels = var.tags
-#   }
-#   spec {
-#     access_modes = [
-#       "ReadWriteOnce"
-#     ]
-#     resources {
-#       requests = {
-#         storage = "1Gi"
-#       }
-#     }
-#     volume_mode = "Filesystem"
-#     volume_name = kubernetes_persistent_volume_v1.efs_pv.metadata.0.name
-#     storage_class_name = kubernetes_storage_class_v1.efs_sc.metadata.0.name
-#   }
-# }
