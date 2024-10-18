816a4cf3250bd04e082e9a78ebde7baeb684da62
Rafael Tomelin <rafael.tomelin@gmail.com>
2024-10-17 23:40:12 -0300

    update

--- terraform/modules/addons/cloudflare/main.tf
+++ terraform/modules/addons/cloudflare/main.tf
@@ -1,12 +1,17 @@
+locals {
+  create_record = length(var.record_name)
+}
+
 
 resource "cloudflare_dns_record" "record" {
   # count = var.record_name != "" ? 1 : 0 && var.record_address != "" ? 1 : 0
-    count = length([for v in [var.record_name, var.record_address] : v if v != ""])
+    # count = length([for v in [var.record_name, var.record_address] : v if v != ""])
+  # count = local.create_record ? 1 : 0
 
 
   zone_id = var.zone_id
   name    = var.record_name
-  content = var.record_address
+  content = "${create_record}-${var.record_address}"
   type    = var.record_type
   proxied = var.record_proxy
   ttl     = var.record_ttl
