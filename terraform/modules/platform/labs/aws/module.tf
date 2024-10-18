e0ae5d12a2916ff43c971e45ae9217613eb8de0b
Rafael Tomelin <rafael.tomelin@gmail.com>
2024-10-17 22:05:05 -0300

    update

--- terraform/modules/platform/labs/aws/module.tf
+++ terraform/modules/platform/labs/aws/module.tf
@@ -1,6 +1,7 @@
 
 module "k8s_cluster" {
   source = "git::https://github.com/kubediscovery/platform-scaffolder.git//terraform/modules/aws/eks_cluster?ref=develop"
+  
 
   project_name          = var.project_name
   cidr_block            = var.cidr_block
--- terraform/modules/platform/labs/azure/module.tf
+++ terraform/modules/platform/labs/azure/module.tf
@@ -1,24 +1,10 @@
 
 
-module "platform" {
-  source = "git::https://github.com/kubediscovery/platform-scaffolder.git//modules/azure/aks_cluster?ref=develop"
+module "k8s_cluster" {
+  source = "git::https://github.com/kubediscovery/platform-scaffolder.git//terraform/modules/azure/aks_cluster?ref=develop"
 
   project_name = var.project_name
   cidr_block   = var.cidr_block
   tags         = var.tags
   region       = var.cloud_provider == "azure" ? "East US" : "East US"
 }
-
-# module "argocd" {
-#   source = "git::https://github.com/kubediscovery/platform-scaffolder.git//modules/addons/argoproj/argocd/?ref=develop"
-
-#   project_name = var.project_name
-#   depends_on   = [module.platform]
-# }
-
-# module "argocd_applications" {
-#   source = "git::https://github.com/kubediscovery/platform-scaffolder.git//modules/addons/argoproj/applications/?ref=develop"
-
-#   depends_on = [module.platform]
-# }
-
