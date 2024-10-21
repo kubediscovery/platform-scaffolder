b04211b5a791a4a57e4473198bf0fa89b5f23fb6
Rafael Tomelin <rafael.tomelin@gmail.com>
2024-10-21 08:44:27 -0300

    update

--- terraform/modules/aws/eks_cluster/volume/storage.tf
+++ terraform/modules/aws/eks_cluster/volume/storage.tf
@@ -63,7 +63,7 @@ resource "kubernetes_persistent_volume_v1" "efs_pv_disk" {
       "ReadWriteOnce",
       "ReadWriteMany"
     ]
-    volume_mode                      = "Disk"
+    volume_mode                      = "Block"
     persistent_volume_reclaim_policy = "Delete"
     storage_class_name               = kubernetes_storage_class_v1.efs_sc.metadata.0.name
     persistent_volume_source {
