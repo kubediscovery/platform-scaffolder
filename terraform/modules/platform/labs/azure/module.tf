985da6a292db23303332ced0201cf10da275a522
Rafael Tomelin <rafael.tomelin@gmail.com>
2024-10-21 18:56:28 -0300

    update

--- terraform/modules/platform/labs/azure/module.tf
+++ terraform/modules/platform/labs/azure/module.tf
@@ -27,21 +27,21 @@ module "addons" {
   }
 }
 
-# module "deployment" {
-#   source = "git::https://github.com/kubediscovery/platform-scaffolder.git//terraform/modules/deployment/init?ref=develop"
+module "deployment" {
+  source = "git::https://github.com/kubediscovery/platform-scaffolder.git//terraform/modules/deployment/init?ref=develop"
 
-#   enabled_addons         = var.enabled_addons
-#   project_name           = var.project_name
-#   cluster_endpoint       = module.k8s_cluster.cluster.endpoint
-#   cluster_ca_certificate = base64decode(module.k8s_cluster.cluster.cluster_ca_certificate)
-#   tags                   = var.tags
-#   cluster_token          = module.k8s_cluster.cluster.token
-#   storage = {
-#     persistent_volume_name = "persistent_volume_name"
-#     storage_class_name     = "azurefile"
-#     persistent_volume_size = "1Gi"
-#   }
-#   # gitops_namespace = module.addons.gitops_namespace
-#   # gitops_source_repos_urls = module.addons.gitops_source_repos_urls
-# }
+  enabled_addons         = var.enabled_addons
+  project_name           = var.project_name
+  cluster_endpoint       = module.k8s_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(module.k8s_cluster.cluster.cluster_ca_certificate)
+  tags                   = var.tags
+  cluster_token          = module.k8s_cluster.cluster.token
+  storage = {
+    persistent_volume_name = "persistent_volume_name"
+    storage_class_name     = "azurefile"
+    persistent_volume_size = "1Gi"
+  }
+  # gitops_namespace = module.addons.gitops_namespace
+  # gitops_source_repos_urls = module.addons.gitops_source_repos_urls
+}
 
