66e9ef1319612e138d1b4df41b5538e506344a21
Rafael Tomelin <rafael.tomelin@gmail.com>
2024-10-21 08:11:25 -0300

    update

--- terraform/modules/deployment/init/output.tf
+++ terraform/modules/deployment/init/output.tf
@@ -1,21 +1,21 @@
-output "arogcd" {
-  value = module.argocd
-}
+# output "arogcd" {
+#   value = module.argocd
+# }
 
-output "publish" {
-  value = module.argocd[0].publish
-}
+# output "publish" {
+#   value = module.argocd[0].publish
+# }
 
-output "http_dns_records" {
-  value = local.http_dns_records
-  description = "values for the http dns records"
-}
-output "home" {
-  value = module.atlantis[0].home
-}
+# output "http_dns_records" {
+#   value = local.http_dns_records
+#   description = "values for the http dns records"
+# }
+# output "home" {
+#   value = module.atlantis[0].home
+# }
 
-output "ingress" {
-  value = {
-    ingress_class_name = module.kong[0].ingress_class_name
-  }
-}
+# output "ingress" {
+#   value = {
+#     ingress_class_name = module.kong[0].ingress_class_name
+#   }
+# }
