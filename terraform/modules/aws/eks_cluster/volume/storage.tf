811acef230d104f2068beb66fc59baca87a83c5d
Rafael Tomelin <rafael.tomelin@gmail.com>
2024-10-21 09:42:48 -0300

    update

--- terraform/modules/aws/eks_cluster/volume/storage.tf
+++ terraform/modules/aws/eks_cluster/volume/storage.tf
@@ -15,12 +15,12 @@ resource "aws_efs_file_system" "this" {
   tags = var.tags
 }
 
-resource "aws_efs_mount_target" "this" {
-  for_each = toset(var.subnet_ids)
-  file_system_id = aws_efs_file_system.this.id
-  subnet_id      = each.value
-  security_groups = var.security_group_ids
-}
+# resource "aws_efs_mount_target" "this" {
+#   for_each = toset(var.subnet_ids)
+#   file_system_id = aws_efs_file_system.this.id
+#   subnet_id      = each.value
+#   security_groups = var.security_group_ids
+# }
 
 
 resource "kubernetes_persistent_volume_v1" "efs_pv" {
