08fd52d3b834eafece364e80ac3ff4593926363c
Rafael Tomelin <rafael.tomelin@gmail.com>
2024-10-21 11:27:21 -0300

    update

---
+++ terraform/modules/deployment/argocd/project.tf
@@ -0,0 +1,82 @@
+locals {
+  dep = argoproj
+}
+resource "kubernetes_manifest" "platform" {
+  manifest = {
+    apiVersion = "argoproj.io/v1alpha1"
+    kind        = "AppProject"
+    metadata = {
+      name      = var.project_name
+      namespace = "argoproj"
+      labels      = var.labels
+    }
+    spec = {
+      clusterResourceWhitelist = [
+        {
+          group = "*"
+          kind  = "*"
+        }
+      ]
+      description = " Deploy of platform"
+      destinations = [
+        {
+          name      = "*"
+          namespace = var.project_name
+          server    = "*"
+        }
+      ]
+      namespaceResourceWhitelist = [
+        {
+          group = "*"
+          kind  = "*"
+        }
+      ]
+      sourceRepos = [
+       base64decode(kubernetes_manifest.repo_argoproj.manifest.data.url),
+       base64decode(kubernetes_manifest.repo_bitnami.manifest.data.url),
+      ]
+    }
+  }
+
+  depends_on = [ local.dep ]
+}
+
+resource "kubernetes_manifest" "platform_shared" {
+  manifest = {
+    apiVersion = "argoproj.io/v1alpha1"
+    kind        = "AppProject"
+    metadata = {
+      name      = "${var.project_name}-shared"
+      namespace = "argoproj"
+      labels      = merge(var.labels, { "app.kubernetes.io/part-of" = "platform" })
+    }
+    spec = {
+      clusterResourceWhitelist = [
+        {
+          group = "*"
+          kind  = "*"
+        }
+      ]
+      description = " Deploy of platform"
+      destinations = [
+        {
+          name      = "*"
+          namespace = "${var.project_name}-shared"
+          server    = "*"
+        }
+      ]
+      namespaceResourceWhitelist = [
+        {
+          group = "*"
+          kind  = "*"
+        }
+      ]
+      sourceRepos = [
+       base64decode(kubernetes_manifest.repo_argoproj.manifest.data.url),
+       base64decode(kubernetes_manifest.repo_bitnami.manifest.data.url),
+      ]
+    }
+  }
+
+  depends_on = [ local.dep ]
+}
