beb47ab1a7bfa03219d1521a785dac8d634f7111
Rafael Tomelin <rafael.tomelin@gmail.com>
2024-10-21 08:09:48 -0300

    update

--- terraform/modules/platform/labs/aws/module.tf
+++ terraform/modules/platform/labs/aws/module.tf
@@ -29,13 +29,31 @@ module "addons" {
   }
 }
 
+module "deployment" {
+  source = "git::https://github.com/kubediscovery/platform-scaffolder.git//terraform/modules/deployment/init?ref=develop"
+
+  enabled_addons         = var.enabled_addons
+  project_name           = var.project_name
+  cluster_endpoint       = module.k8s_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(module.k8s_cluster.cluster.cluster_ca_certificate)
+  tags                   = var.tags
+  cluster_token          = module.k8s_cluster.cluster.token
+  storage = {
+    persistent_volume_name = "persistent_volume_name"
+    storage_class_name     = "azurefile"
+    persistent_volume_size = "1Gi"
+  }
+  gitops_namespace = module.addons.gitops_namespace
+  gitops_source_repos_urls = module.addons.gitops_source_repos_urls
+}
+
 module "switch_cloud_provider" {
-  source           = "git::https://github.com/kubediscovery/platform-scaffolder.git//terraform/modules/switch/init?ref=develop"
-  project_name     = var.project_name
-  tags             = var.tags
-  http_dns_records = module.addons.http_dns_records
-  cloudflare_api_token   = var.cloudflare_api_token
-  cloudflare_zone_id     = var.cloudflare_zone_id
+  source               = "git::https://github.com/kubediscovery/platform-scaffolder.git//terraform/modules/switch/init?ref=develop"
+  project_name         = var.project_name
+  tags                 = var.tags
+  http_dns_records     = module.addons.http_dns_records
+  cloudflare_api_token = var.cloudflare_api_token
+  cloudflare_zone_id   = var.cloudflare_zone_id
 
 }
 
