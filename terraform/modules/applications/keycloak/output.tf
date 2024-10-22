9bab221c8bab92be9437dae1a2d8d1b4e178204e
Rafael Tomelin <rafael.tomelin@gmail.com>
2024-10-22 09:30:20 -0300

    update

--- terraform/modules/applications/keycloak/output.tf
+++ terraform/modules/applications/keycloak/output.tf
@@ -8,5 +8,6 @@ output "publish_debug" {
   value = {
     name      = kubernetes_manifest.keycloak.manifest.metadata.name
     namespace = kubernetes_manifest.keycloak.manifest.metadata.namespace
+    address = data.kubernetes_ingress_v1.keycloak.status.0.load_balancer.0.ingress.0.ip
   }
 }
