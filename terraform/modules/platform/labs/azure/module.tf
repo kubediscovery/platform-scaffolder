f035e08d09accde4763e3b05ca92b75c42ebbfec
Rafael Tomelin <rafael.tomelin@gmail.com>
2024-10-22 11:24:27 -0300

    update

--- terraform/modules/platform/labs/azure/module.tf
+++ terraform/modules/platform/labs/azure/module.tf
@@ -45,11 +45,11 @@ module "applications" {
   # gitops_source_repos_urls = module.addons.gitops_source_repos_urls
 }
 
-module "dns" {
-  source               = "git::https://github.com/kubediscovery/platform-scaffolder.git//terraform/modules/dns/init?ref=develop"
-  project_name         = var.project_name
-  tags                 = var.tags
-  http_dns_records     = module.applications.http_dns_records
-  cloudflare_api_token = var.cloudflare_api_token
-  cloudflare_zone_id   = var.cloudflare_zone_id
-}
+# module "dns" {
+#   source               = "git::https://github.com/kubediscovery/platform-scaffolder.git//terraform/modules/dns/init?ref=develop"
+#   project_name         = var.project_name
+#   tags                 = var.tags
+#   http_dns_records     = module.applications.http_dns_records
+#   cloudflare_api_token = var.cloudflare_api_token
+#   cloudflare_zone_id   = var.cloudflare_zone_id
+# }
