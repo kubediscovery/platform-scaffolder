f86f80f34e62356b0bdeac2d79db931e8d072ea7
Rafael Tomelin <rafael.tomelin@gmail.com>
2024-10-22 09:03:05 -0300

    update

--- terraform/modules/applications/keycloak/manifest.tf
+++ terraform/modules/applications/keycloak/manifest.tf
@@ -50,7 +50,7 @@ resource "kubernetes_manifest" "keycloak" {
 
 data "kubernetes_ingress_v1" "keycloak" {
   metadata {
-    name      = local.release_name
-    namespace = local.release_namespace
+    name      = kubernetes_manifest.keycloak.metadata.0.name
+    namespace = kubernetes_manifest.keycloak.metadata.0.namespace
   }
 }
