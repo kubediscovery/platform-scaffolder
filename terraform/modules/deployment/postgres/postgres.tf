b2e9b7f839f534d03d86542e0885ba4e91afd3aa
Rafael Tomelin <rafael.tomelin@gmail.com>
2024-10-22 08:04:07 -0300

    update

--- terraform/modules/deployment/postgres/postgres.tf
+++ terraform/modules/deployment/postgres/postgres.tf
@@ -1,4 +1,4 @@
-resource "kubernetes_manifest" "postgres" {
+resource "kubernetes_manifest" "keycloak" {
   manifest = {
     apiVersion = "argoproj.io/v1alpha1"
     kind       = "Application"
@@ -25,6 +25,39 @@ resource "kubernetes_manifest" "postgres" {
         chart          = "keycloak"
         repoURL        = "https://charts.bitnami.com/bitnami"
         targetRevision = "24.0.1"
+        
+      }
+    }
+  }
+}
+
+resource "kubernetes_manifest" "postgres" {
+  manifest = {
+    apiVersion = "argoproj.io/v1alpha1"
+    kind       = "Application"
+    metadata = {
+      name      = "postgres"
+      namespace = "argoproj"
+      labels    = var.labels
+    }
+    spec = {
+      syncPolicy = {
+        automated = {
+          prune    = "true"
+          selfHeal = "true"
+        }
+        syncOptions = [
+        "CreateNamespace=true"]
+      }
+      destination = {
+        namespace = "platform-shared"
+        server    = "https://kubernetes.default.svc"
+      }
+      project = "platform-shared"
+      source = {
+        chart          = "postgres"
+        repoURL        = "https://charts.bitnami.com/bitnami"
+        targetRevision = "16.0.4"
         
       }
     }
