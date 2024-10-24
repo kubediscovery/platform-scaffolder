dafc2f0afcc88db53dd2bab69d79f36fd00fcdb7
Rafael Tomelin <rafael.tomelin@gmail.com>
2024-10-24 08:49:01 -0300

    update

--- terraform/modules/applications/postgres/locals.tf
+++ terraform/modules/applications/postgres/locals.tf
@@ -3,10 +3,4 @@ locals {
   chart_version     = "16.0.4"
   release_name      = "postgresql"
   release_namespace = "platform-shared"
-
-  publish = {
-    "name"    = data.kubernetes_ingress_v1.keycloak.spec == null ? "" : data.kubernetes_ingress_v1.keycloak.spec.0.rule.0.host
-    "address" = data.kubernetes_ingress_v1.keycloak.status == null ? "" : data.kubernetes_ingress_v1.keycloak.status.0.load_balancer.0.ingress.0.hostname != "" ? data.kubernetes_ingress_v1.keycloak.status.0.load_balancer.0.ingress.0.hostname : data.kubernetes_ingress_v1.keycloak.status.0.load_balancer.0.ingress.0.ip
-    "type"    = data.kubernetes_ingress_v1.keycloak.status == null ? "" : data.kubernetes_ingress_v1.keycloak.status.0.load_balancer.0.ingress.0.ip != "" ? "address" : "hostname"
-  }
 }
