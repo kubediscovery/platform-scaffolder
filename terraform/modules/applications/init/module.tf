3b07fd0af8c2acdc694808035dcdb50fa0f9b05e
Rafael Tomelin <rafael.tomelin@gmail.com>
2024-10-24 19:56:43 -0300

    update

--- terraform/modules/applications/init/module.tf
+++ terraform/modules/applications/init/module.tf
@@ -10,29 +10,29 @@
 # }
 
 
-module "keycloak" {
-  source = "git::https://github.com/kubediscovery/platform-scaffolder.git//terraform/modules/applications/keycloak/?ref=develop"
-  # count  = local.enabled_addons.argoproj_argocd.enabled ? 1 : 0
+# module "keycloak" {
+#   source = "git::https://github.com/kubediscovery/platform-scaffolder.git//terraform/modules/applications/keycloak/?ref=develop"
+#   # count  = local.enabled_addons.argoproj_argocd.enabled ? 1 : 0
 
-  labels        = var.tags
-  project_name  = var.project_name
-  # ingress_class = var.ingress_class_name
-  storage = var.storage
-  # gitops_namespace = var.gitops_namespace
-  # gitops_source_repos_urls = var.gitops_source_repos_urls
-}
+#   labels        = var.tags
+#   project_name  = var.project_name
+#   # ingress_class = var.ingress_class_name
+#   storage = var.storage
+#   # gitops_namespace = var.gitops_namespace
+#   # gitops_source_repos_urls = var.gitops_source_repos_urls
+# }
 
-module "postgresql" {
-  source = "git::https://github.com/kubediscovery/platform-scaffolder.git//terraform/modules/applications/postgres/?ref=develop"
-  # count  = local.enabled_addons.argoproj_argocd.enabled ? 1 : 0
+# module "postgresql" {
+#   source = "git::https://github.com/kubediscovery/platform-scaffolder.git//terraform/modules/applications/postgres/?ref=develop"
+#   # count  = local.enabled_addons.argoproj_argocd.enabled ? 1 : 0
 
-  labels        = var.tags
-  project_name  = var.project_name
-  # ingress_class = var.ingress_class_name
-  storage = var.storage
-  # gitops_namespace = var.gitops_namespace
-  # gitops_source_repos_urls = var.gitops_source_repos_urls
-}
+#   labels        = var.tags
+#   project_name  = var.project_name
+#   # ingress_class = var.ingress_class_name
+#   storage = var.storage
+#   # gitops_namespace = var.gitops_namespace
+#   # gitops_source_repos_urls = var.gitops_source_repos_urls
+# }
 
 module "app_of_apps" {
   source = "git::https://github.com/kubediscovery/platform-scaffolder.git//terraform/modules/applications/appOfApps/?ref=develop"
