c28a13023ba6065dc84e7cbe070ff05f30e30520
Rafael Tomelin <rafael.tomelin@gmail.com>
2024-10-24 20:02:24 -0300

    update

--- terraform/modules/platform/labs/azure/output.tf
+++ terraform/modules/platform/labs/azure/output.tf
@@ -14,13 +14,13 @@ output "project_name" {
 #   value = module.addons
 # }
 
-output "publish" {
-  value = module.applications.http_dns_records
-}
+# output "publish" {
+#   value = module.applications.http_dns_records
+# }
 
-output "publish_debug" {
-  value = module.applications.http_dns_records_debug
-}
+# output "publish_debug" {
+#   value = module.applications.http_dns_records_debug
+# }
 
 # output "home" {
 #   value = module.addons.home
