daba6d2b89bba7cdf93786f739e22fd6f7949769
Rafael Tomelin <rafael.tomelin@gmail.com>
2024-10-25 09:19:18 -0300

    update

--- terraform/modules/applications/appOfApps/project.tf
+++ terraform/modules/applications/appOfApps/project.tf
@@ -40,6 +40,7 @@ resource "kubernetes_manifest" "platform" {
       sourceRepos = [
         base64decode(kubernetes_manifest.repo_argoproj.manifest.data.url),
         base64decode(kubernetes_manifest.repo_bitnami.manifest.data.url),
+        base64decode(kubernetes_manifest.repo_newrelic.manifest.data.url),
         "git@github.com:kubediscovery/platform-infrastrucutre.git"
       ]
     }
@@ -83,6 +84,7 @@ resource "kubernetes_manifest" "platform_shared" {
       sourceRepos = [
         base64decode(kubernetes_manifest.repo_argoproj.manifest.data.url),
         base64decode(kubernetes_manifest.repo_bitnami.manifest.data.url),
+        base64decode(kubernetes_manifest.repo_newrelic.manifest.data.url),
         "git@github.com:kubediscovery/platform-infrastrucutre.git"
       ]
     }
