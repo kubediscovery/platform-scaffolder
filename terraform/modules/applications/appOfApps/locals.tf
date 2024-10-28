067dcff31c3f5d6dd3dedd97e4673029adce2390
Rafael Tomelin <rafael.tomelin@gmail.com>
2024-10-28 19:06:30 -0300

    update

--- terraform/modules/applications/appOfApps/locals.tf
+++ terraform/modules/applications/appOfApps/locals.tf
@@ -2,6 +2,7 @@ locals {
   sourceRepos = [
     base64decode(kubernetes_manifest.repo_argoproj.manifest.data.url),
     base64decode(kubernetes_manifest.repo_bitnami.manifest.data.url),
+    base64decode(kubernetes_manifest.repo_exsecrets.manifest.data.url),
     "git@github.com:kubediscovery/platform-infrastrucutre.git"
   ]
 }
