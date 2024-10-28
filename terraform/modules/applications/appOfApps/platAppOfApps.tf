5c0ad5345b801ed95adb8466d203388a5f94805c
Rafael Tomelin <rafael.tomelin@gmail.com>
2024-10-28 08:56:44 -0300

    update

--- terraform/modules/applications/appOfApps/platAppOfApps.tf
+++ terraform/modules/applications/appOfApps/platAppOfApps.tf
@@ -16,7 +16,7 @@ resource "kubernetes_manifest" "argo_application" {
     apiVersion = "argoproj.io/v1alpha1"
     kind       = "Application"
     metadata = {
-      name      = "plat-app-of-apps"
+      name      = "app-of-apps-platform"
       namespace = "argoproj"
       annotations = {
            "argocd.argoproj.io/sync-wave" = "3"
