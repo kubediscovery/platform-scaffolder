e1f47976033163dd57fc08ad6f9d3fe61b6099f8
Rafael Tomelin <rafael.tomelin@gmail.com>
2024-10-28 20:08:07 -0300

    update

--- terraform/modules/azure/aks_cluster/modules/sp/main.tf
+++ terraform/modules/azure/aks_cluster/modules/sp/main.tf
@@ -4,11 +4,11 @@ data "azuread_client_config" "current" {}
 resource "azuread_application" "this" {
   display_name = "external-secrets-app"
   owners       = data.azuread_client_config.current.object_id
-  
+
 }
 
 resource "azuread_service_principal" "this" {
-  client_id                    = azuread_application.this.application_id
+  client_id                    = azuread_application.this.client_id
   owners                       = data.azuread_client_config.current.object_id
   app_role_assignment_required = false
 }
