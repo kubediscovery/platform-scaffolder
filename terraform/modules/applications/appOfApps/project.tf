720b578fdb3f357e4e417d5f871e57d962ded9dc
Rafael Tomelin <rafael.tomelin@gmail.com>
2024-10-28 19:04:37 -0300

    update

--- terraform/modules/applications/appOfApps/project.tf
+++ terraform/modules/applications/appOfApps/project.tf
@@ -29,6 +29,11 @@ resource "kubernetes_manifest" "platform" {
           name      = "*"
           namespace = "argoproj"
           server    = "*"
+        },
+{
+          name      = "*"
+          namespace = "exsecrets-system"
+          server    = "*"
         }
       ]
       namespaceResourceWhitelist = [
