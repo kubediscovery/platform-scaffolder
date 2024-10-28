42ac43d817f6131fffb6197df746e75ce8a3e4ac
Rafael Tomelin <rafael.tomelin@gmail.com>
2024-10-28 08:29:16 -0300

    update

--- terraform/modules/applications/appOfApps/locals.tf
+++ terraform/modules/applications/appOfApps/locals.tf
@@ -3,6 +3,7 @@ locals {
     base64decode(kubernetes_manifest.repo_argoproj.manifest.data.url),
     base64decode(kubernetes_manifest.repo_bitnami.manifest.data.url),
     base64decode(kubernetes_manifest.repo_newrelic.manifest.data.url),
+    "https://github.com/kubernetes-sigs/gateway-api.git",
     "git@github.com:kubediscovery/platform-infrastrucutre.git"
   ]
 }
