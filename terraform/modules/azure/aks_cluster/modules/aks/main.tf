e75a66e513f87dadd2e8f7517d8e34bec6701ab1
Rafael Tomelin <rafael.tomelin@gmail.com>
2024-10-29 15:49:53 -0300

    update

--- terraform/modules/azure/aks_cluster/modules/aks/main.tf
+++ terraform/modules/azure/aks_cluster/modules/aks/main.tf
@@ -59,7 +59,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   tags = var.tags
 
   lifecycle {
-    create_before_destroy = true
+    create_before_destroy = false
     ignore_changes = [
       tags["created_at"]
     ]
