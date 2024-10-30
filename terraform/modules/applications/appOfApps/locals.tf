b5686a072f12327e3de939caf415c6c59f6cbfec
Rafael Tomelin <rafael.tomelin@gmail.com>
2024-10-30 20:07:25 -0300

    update

--- terraform/modules/applications/appOfApps/locals.tf
+++ terraform/modules/applications/appOfApps/locals.tf
@@ -5,7 +5,7 @@ locals {
     base64decode(kubernetes_manifest.repo_exsecrets.manifest.data.url),
     base64decode(kubernetes_manifest.repo_konga.manifest.data.url),
     base64decode(kubernetes_manifest.repo_kong.manifest.data.url),
-    base64decode(kubernetes_manifest.repo_platform_infrastrucutre.manifest.data.url),
+    base64decode(kubernetes_manifest.repo_kd_helm.manifest.data.url),
     "git@github.com:kubediscovery/platform-infrastrucutre.git"
   ]
 
